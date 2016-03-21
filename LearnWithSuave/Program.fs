f1a7ad25b5413f387f09e6c31c1001514684e161
Denys Kholod <denyskh@outlook.com>
2016-03-21 18:56:13 +0200

    create F# console app with Suave host

---
+++ LearnWithSuave/Program.fs
@@ -0,0 +1,30 @@
+﻿open System 
+
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Suave.RequestErrors
+
+let echo =
+    request (fun r ->
+        match r.queryParam "msg" with
+        | Choice1Of2 toEcho -> OK toEcho
+        | Choice2Of2 msg -> BAD_REQUEST msg)
+
+let app =
+  choose
+    [ GET >=> choose
+        [ 
+            path "/healthcheck" >=> OK "OK"
+            path "/echo" >=> echo 
+        ]
+    ]
+
+[<EntryPoint>]
+let main argv = 
+    
+    printfn "Starting web server..."
+    
+    startWebServer defaultConfig app
+    0
