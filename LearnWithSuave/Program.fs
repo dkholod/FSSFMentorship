04faf4346c0695a41baff9dfb78467949a1222b1
Denys Kholod <denyskh@outlook.com>
2016-03-23 12:41:25 +0200

    add count function to calculate page length by url

--- LearnWithSuave/Program.fs
+++ LearnWithSuave/Program.fs
@@ -1,30 +1,43 @@
 ﻿open System 
 
+open System.Net.Http
+
 open Suave
 open Suave.Filters
 open Suave.Operators
 open Suave.Successful
 open Suave.RequestErrors
 
-let echo =
-    request (fun r ->
-        match r.queryParam "msg" with
-        | Choice1Of2 toEcho -> OK toEcho
-        | Choice2Of2 msg -> BAD_REQUEST msg)
+// toOK: HttpContext-> 'a -> Async<HttpContext option>
+let toOK (x:HttpContext) s = 
+    OK (s.ToString()) x
+
+let httpClient = new HttpClient()
+
+let echo = request (fun r -> 
+    match r.queryParam "msg" with
+    | Choice1Of2 toEcho -> OK toEcho
+    | Choice2Of2 msg -> BAD_REQUEST msg)
+
+let count: WebPart = fun (x : HttpContext) -> async {
+        match x.request.queryParam "url" with
+        | Choice2Of2 msg -> return! BAD_REQUEST msg x
+        | Choice1Of2 url -> let! page = httpClient.GetStringAsync(url) |> Async.AwaitTask
+                            return! page.Length |> toOK x
+}
 
 let app =
   choose
     [ GET >=> choose
         [ 
             path "/healthcheck" >=> OK "OK"
-            path "/echo" >=> echo 
+            path "/echo" >=> echo
+            path "/count" >=> count
         ]
     ]
 
 [<EntryPoint>]
 let main argv = 
-    
     printfn "Starting web server..."
-    
     startWebServer defaultConfig app
     0
